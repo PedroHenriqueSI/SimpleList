ef1b009ab0ef4834ae263de759081a6dca86635a
PedroHenriqueSI <57466217+PedroHenriqueSI@users.noreply.github.com>
2020-09-22 22:41:34 -0300

    Finalizando projeto Lista simples

--- Lista/Program.cs
+++ Lista/Program.cs
@@ -12,14 +12,15 @@ namespace Lista
             List<People> list = new List<People>();
             list.Add(new People("Pedro", 12));
             list.Add(new People("Carlos", 15));
-            list.Add(new People("Leane", 18));
+            list.Add(new People("Junior", 18));
             list.Add(new People("Claudia", 30));
             list.Add(new People("Snow", 19));
 
             foreach (People people in list)
             {
-                Console.WriteLine("Name: "+people.Name+"-- Age: "+people.Age);
+                Console.WriteLine("Name: " + people.Name + "-- Age: " + people.Age);
             }
+            Console.WriteLine("Total: "+list.Count.ToString());
         }
     }
 }
