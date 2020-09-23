0cd41d040954df8f3011e88320f18d52ed901c33
PedroHenriqueSI <57466217+PedroHenriqueSI@users.noreply.github.com>
2020-09-22 22:18:45 -0300

    Instanciando a Lista manualmente

--- Lista/Program.cs
+++ Lista/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Lista.Entities;
 using System;
 using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Lista
 {
@@ -9,21 +10,15 @@ namespace Lista
         static void Main(string[] args)
         {
             List<People> list = new List<People>();
+            list.Add(new People("Pedro", 12));
+            list.Add(new People("Carlos", 15));
+            list.Add(new People("Leane", 18));
+            list.Add(new People("Claudia", 30));
+            list.Add(new People("Snow", 19));
 
-            Console.Write("Enter the number of people");
-            int n = int.Parse(Console.ReadLine());
-
-            for (int i = 1; i <= n; i++)
-            {
-                Console.Write("Name of "+i+" people: ");
-                string name = Console.ReadLine();
-                Console.Write("Age: ");
-                int age = int.Parse(Console.ReadLine());
-                list.Add(new People(name, age));
-            }
             foreach (People people in list)
             {
-                Console.WriteLine(people.Name+" - "+people.Age);
+                Console.WriteLine("Name: "+people.Name+"-- Age: "+people.Age);
             }
         }
     }
