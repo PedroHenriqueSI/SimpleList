f638b8e742aa2cdfc7e2e6c018be3a48702c3fa1
PedroHenriqueSI <57466217+PedroHenriqueSI@users.noreply.github.com>
2020-09-22 22:10:12 -0300

    Iniciando projeto de lista

---
+++ Lista/Entities/People.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lista.Entities
+{
+    class People
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public People()
+        {
+        }
+
+        public People(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
---
+++ Lista/Program.cs
@@ -0,0 +1,30 @@
+﻿using Lista.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Lista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<People> list = new List<People>();
+
+            Console.Write("Enter the number of people");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write("Name of "+i+" people: ");
+                string name = Console.ReadLine();
+                Console.Write("Age: ");
+                int age = int.Parse(Console.ReadLine());
+                list.Add(new People(name, age));
+            }
+            foreach (People people in list)
+            {
+                Console.WriteLine(people.Name+" - "+people.Age);
+            }
+        }
+    }
+}
